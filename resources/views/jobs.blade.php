58ae6e4d1c654b0b393aea6a6253e4ecd277b049
Alperen <alperenkenan40@gmail.com>
2024-09-20 13:57:37 +0200

    Epsiode 7 completed

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,7 +5,7 @@
     <ul>
         @foreach($jobs as $job)
             <li class="py-4">
-                <a href="/jobs/ {{$job['id']}}" class="text-blue-500 hover:underline">
+                <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
                     <h3 class="text-xl">{{$job['title']}}</h3>
                     <p>{{$job['salary']}}</p>
                 </a>
