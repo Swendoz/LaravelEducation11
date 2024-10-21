0b932032ef692c5aacd84af719826877026dc860
Hyper <alperenkenan40@gmail.com>
2024-10-21 11:00:00 +0200

    Episode 21 completed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 //Route::get('/', function () {
 //    return view('welcome', [
@@ -16,5 +18,13 @@ Route::view('/contact', 'contact');
 
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+
+Route::get('login', [SessionController::class, 'create']);
+Route::post('login', [SessionController::class, 'store']);
 
 
